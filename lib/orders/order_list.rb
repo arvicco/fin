87a2e5f8aa0e554419297a593ec851f3ef0869a2
arvicco <arvitallian@gmail.com>
2011-03-31 15:40:26 +0400

    EventedDataStream slimmed down, RevisionTracking and FileSaving mixins extracted

--- lib/orders/order_list.rb
+++ lib/orders/order_list.rb
@@ -13,5 +13,17 @@ module Orders
     def index item
       item.id
     end
+
+    def add_record(rec)
+      isin_id = rec.GetValAsLong('isin_id')
+      order = Orders::Order.new :isin_id => isin_id,
+                                :id => rec.GetValAsString('replID').to_i,
+                                :rev => rec.GetValAsString('replRev').to_i,
+                                :price => rec.GetValAsString('price').to_f,
+                                :volume => rec.GetValAsString('volume').to_f,
+                                :dir => rec.GetValAsLong('dir')
+      add? order
+    end
+
   end
 end
