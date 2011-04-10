39e1e40059422499665bed224c9abe7e68f27b9c
arvicco <arvitallian@gmail.com>
2011-04-10 18:22:13 +0400

    Fin requires and references fixed

--- lib/fin.rb
+++ lib/fin.rb
@@ -1,9 +1,5 @@
 require 'version'
-require 'rubygems'
-require 'bundler/setup'
-Bundler.require(:default)
 
-require 'fin/container_list'
 require 'fin/order_list'
 require 'fin/deal_list'
 require 'fin/models/deal'
